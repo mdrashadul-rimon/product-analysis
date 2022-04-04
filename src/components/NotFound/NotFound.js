69ec364f949c7a8344dfdf38facd024a71710319
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-04-05 01:30:49 +0600

    fancy error 404 page created

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2 className='text-4xl'>The page you are looking for is not found.</h2>
-        </div>
+        <section className="page_404">
+            <div className="md:container">
+                
+                    <div className="grid grid-cols-1 ">
+                        <div className="text-center">
+                            <div className="four_zero_four_bg">
+                                <h1 className="text-center ">404</h1>
+                            </div>
+
+                            <div className="contant_box_404">
+                                <h3 className="h2">Look like you're lost</h3>
+                                <p>the page you are looking for not available!</p>
+                                <Link to={'/'} className='link_404'>Go Back Home</Link>
+                            </div>
+                        </div>
+                    </div>
+                
+            </div>
+        </section>
     );
 };
 

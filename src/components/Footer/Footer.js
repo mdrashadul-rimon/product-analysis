5cd8defc2c6cf2e2ede167784e18b4cd348cd1a2
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-04-04 20:54:02 +0600

    added blog section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='py-1 mt-5 static bottom-0 h-10 w-full bg-black text-white'><p className='text-xl'>All Rights Reserved. Rashadul Alam Rimon | 2022</p></footer>
+        <footer className='py-1 mt-5 static bottom-0 h-10 w-full bg-black text-white'><p className='text-lg'>All Rights Reserved. Rashadul Alam Rimon | 2022</p></footer>
     );
 };
 

305eccadd8313a6d416ccbb38dc0e702d1fa0f8d
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-04-05 00:22:11 +0600

    about section updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,10 @@ const About = () => {
 
     return (
         <div>
-            <h2 className='text-4xl'>This is about section.</h2>
+            <h2 className='text-4xl'>Developers Portfolio</h2>
+            <p className='text-lg'>Name: Md. Rashadul Alam</p>
+            <p className='text-lg'>Education: B.Sc in CSE (BAUET)</p>
+            <p className='text-lg'>Nationality: Bangladeshi</p>
             
         </div>
     );
